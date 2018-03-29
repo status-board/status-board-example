58b7baf02186a77b32c01abe498492bcabe19a6d
James W. Lane <james.w.lane@mac.com>
2018-03-29 09:39:23 -0400

    docs(start): Added the filter object for testing

--- start.js
+++ start.js
@@ -4,7 +4,13 @@ const statusBoard = require('status-board').default;
 statusBoard(
   {
     port: process.env.ATLASBOARD_PORT || 3000,
-    install: false
+    install: false,
+    // filters: {
+    // jobFilter: 'picture',
+    // jobFilter: 'pic(.*)day',
+    // dashboardFilter: 'myfirst',
+    // dashboardFilter: 'my(.*)board',
+    // },
   },
   function (err) {
     if (err) {
