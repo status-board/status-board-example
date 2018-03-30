c7b25a09f06aa304d86d2a1d2f587afeb0a43cf4
James W. Lane <james.w.lane@mac.com>
2018-03-29 23:11:29 -0400

    ci(aws): Tweaking AWS config

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const statusBoard = require('status-board').default;
 
 statusBoard(
   {
-    port: process.env.ATLASBOARD_PORT || 3000,
+    port: process.env.PORT || 3000,
     install: false,
     // filters: {
     // jobFilter: 'picture',
